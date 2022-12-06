0eb66befe32ce648a5efca13f439ae6af0d66951
vidvukobratovic <65481617+vidvukobratovic@users.noreply.github.com>
2022-12-05 21:20:59 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>LAB10 PART C</h1>
+        <h1>LAB10 PART C </h1>
 
-        <p>I used the JavaScript Framework React to build this website. I used online hosting service Netlify to deploy it. I used the following steps: </p> <br>
-        <p>1. Install Node.js</p>
       </header>
     </div>
   );
