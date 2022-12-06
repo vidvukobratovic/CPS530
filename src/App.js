fbb6614bc386ccd3fab26cda551808664e792e35
vidvukobratovic <65481617+vidvukobratovic@users.noreply.github.com>
2022-12-05 21:23:51 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
         <h1>LAB10 PART C </h1>
     
         <div> This is a react app I deployed using a service called Netlify </div>
